ffc9056d57e8b5134344c5292257191f38c6469a
vicvictor86 <vicbem@hotmail.com>
2022-06-16 00:29:04 -0300

    Project made to suport typeorm V2

---
+++ ormconfig.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "default",
+    type: "postgres",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [process.env.NODE_ENV === "production" 
+      ? "./dist/modules/**/infra/typeorm/entities/*.js" 
+      : "./src/modules/**/infra/typeorm/entities/*.ts"
+    ],
+    migrations: [process.env.NODE_ENV === "production" 
+      ? "./dist/shared/infra/typeorm/migrations/*.js"
+      : "./src/shared/infra/typeorm/migrations/*.ts"
+    ],
+    cli: {migrationsDir: process.env.NODE_ENV === "production" 
+      ? "./dist/shared/infra/typeorm/migrations" 
+      : "./src/shared/infra/typeorm/migrations"
+    }
+  }
